e8bc3b5c890cdc4710258e416a78e186a6777034
yuvnishkumar1993-png <yuvnishkumar1993@gmail.com>
2026-08-10 18:34:22 +0530

    Update option_chain.py

--- pages/option_chain.py
+++ pages/option_chain.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 import pandas as pd
 from dhan_api import get_fno_symbols_list, fetch_expiry_and_lots_universal
